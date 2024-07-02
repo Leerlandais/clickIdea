24659afdc949e2f20e2bddebdd69f444b067ad9e
leerlandais <leerlandais@gmail.com>
2024-07-02 20:03:14 +0100

    see where this takes me...

--- scripts/mainGameScript.js
+++ scripts/mainGameScript.js
@@ -1,5 +1,8 @@
-const mainGameButton = document.getElementById('mainGameButton');
+const mainGameButton = document.getElementById('mainGameButton'),
+      clickCounterSpan = document.getElementById('clickCounterSpan');
+const starterHints = document.getElementById("starterHints");
 
+starterHints.textContent = "Get to 20 Clicks";
 let currentClickCount = 0;
 
 
@@ -10,6 +13,13 @@ mainGameButton.addEventListener("click", userClickCounter);
 function userClickCounter() {
     currentClickCount++;
     console.log(currentClickCount);
+    clickCounterSpan.textContent = currentClickCount.toString();
+
+    if (currentClickCount >= 20) {
+        alert("Well done!")
+        window.location.reload();
+
+    }
 }
 
 
