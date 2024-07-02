3b5eaa4153df4378e1e2a121bf6eb9e14bc9523a
leerlandais <leerlandais@gmail.com>
2024-07-02 20:26:35 +0100

    localStorage OK

--- scripts/mainGameScript.js
+++ scripts/mainGameScript.js
@@ -4,6 +4,15 @@ const starterHints = document.getElementById("starterHints");
 
 starterHints.textContent = "Get to 20 Clicks";
 let currentClickCount = 0;
+let fetchClickCount = localStorage.getItem('clickCount');
+console.log(fetchClickCount);
+
+
+if (!fetchClickCount) {
+    clickCounterSpan.textContent = "You need to click a bit!";
+}else {
+    clickCounterSpan.textContent = fetchClickCount;
+}
 
 
 mainGameButton.addEventListener("click", userClickCounter);
@@ -12,7 +21,7 @@ mainGameButton.addEventListener("click", userClickCounter);
 
 function userClickCounter() {
     currentClickCount++;
-    console.log(currentClickCount);
+    localStorage.setItem('clickCount', currentClickCount);
     clickCounterSpan.textContent = currentClickCount.toString();
 
     if (currentClickCount >= 20) {
