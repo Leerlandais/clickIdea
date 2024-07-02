69fe7fb005a9b9cd896b1be21cc5df69f59e42e0
leerlandais <leerlandais@gmail.com>
2024-07-02 21:33:28 +0100

    basuc localStorage : OK

--- scripts/mainGameScript.js
+++ scripts/mainGameScript.js
@@ -4,13 +4,16 @@ const starterHints = document.getElementById("starterHints");
 
 starterHints.textContent = "Get to 20 Clicks";
 let currentClickCount = 0;
+let openshop = 20;
 let fetchClickCount = localStorage.getItem('clickCount');
 console.log(fetchClickCount);
 
 
 if (!fetchClickCount) {
     clickCounterSpan.textContent = "You need to click a bit!";
+    }else if  (fetchClickCount >= openshop) {
 }else {
+    currentClickCount = fetchClickCount;
     clickCounterSpan.textContent = fetchClickCount;
 }
 
@@ -21,11 +24,11 @@ mainGameButton.addEventListener("click", userClickCounter);
 
 function userClickCounter() {
     currentClickCount++;
-    localStorage.setItem('clickCount', currentClickCount);
+    localStorage.setItem('clickCount', currentClickCount.toString());
     clickCounterSpan.textContent = currentClickCount.toString();
 
-    if (currentClickCount >= 20) {
-        alert("Well done!")
+    if (currentClickCount >= openshop) {
+        alert("Well done! The shop is now available!")
         window.location.reload();
 
     }
