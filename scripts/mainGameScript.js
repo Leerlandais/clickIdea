4ed2a324534a1bad3d84bd38d9c024fc5b667d4e
leerlandais <leerlandais@gmail.com>
2024-07-03 07:15:43 +0100

    update by time : OK

--- scripts/mainGameScript.js
+++ scripts/mainGameScript.js
@@ -1,7 +1,7 @@
 const mainGameButton = document.getElementById('mainGameButton'),
       clickCounterSpan = document.getElementById('clickCounterSpan');
 const starterHints = document.getElementById("starterHints");
-
+/*
 starterHints.textContent = "Get to 20 Clicks";
 let currentClickCount = 0;
 let openshop = 20;
@@ -33,5 +33,28 @@ function userClickCounter() {
 
     }
 }
+*/
+/*
+let clickCountTest = 10;
+let clickSpeedTest = 3;
+let currentTimeTest = Date.now()
+
+localStorage.setItem('clickCountTest', clickCountTest.toString());
+localStorage.setItem('currentTimeTest', currentTimeTest.toString());
+localStorage.setItem('clickSpeedTest', clickSpeedTest.toString());
+
+ */
 
 
+let gameLaunchTime = Date.now();
+let lastGameTime = localStorage.getItem('currentTimeTest');
+let timeDiff = gameLaunchTime - lastGameTime;
+let speedTest = localStorage.getItem('clickSpeedTest');
+timeDiff = Math.round(timeDiff / 60000);
+console.log(timeDiff+" : timeDiff");
+let bonusClicks = speedTest * timeDiff;
+console.log("bonus "+ bonusClicks);
+let countTest = localStorage.getItem('clickCountTest');
+let newCount = (parseInt(countTest) + parseInt(bonusClicks));
+console.log("newCount: "+ newCount);
+// alert("You previously had " + countTest + " clicks. You now have " + newCount + " clicks");
