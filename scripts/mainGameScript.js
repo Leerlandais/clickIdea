4a22ee9cf434909a689a51e801f3d40cbfb23045
leerlandais <leerlandais@gmail.com>
2024-07-03 18:22:25 +0100

    shop now opens, just needs options

--- scripts/mainGameScript.js
+++ scripts/mainGameScript.js
@@ -2,17 +2,19 @@ const mainGameButton = document.getElementById('mainGameButton'),
       clickCounterSpan = document.getElementById('clickCounterSpan'),
       clickCounterAll = document.getElementById('clickCounterAll'),
       clickCounterSpeed = document.getElementById('clickCounterSpeed'),
-      starterHints = document.getElementById("starterHints");
+      starterHints = document.getElementById("starterHints"),
+      shopHolder = document.querySelector(".shopHolder");
 
 let userClickCount = 0,
     userClickReset = 0,
     recordCount =  0,
     clickCount = 0,
-    clickSpeed = 0;
+    clickSpeed = 0,
+    speedBoost = [0,0,0,0,0,0,0,0]; // Malheureusement, je ne connais pas encore assez d'Objet Orienté en JS pour le tenter - à revoir bientôt
 
 
 
-// Si jeu est sur un nouvelle ordi/navigator, création de Storage
+// Si jeu est sur un nouvelle ordi/navigator, création de Storage pour eviter d'erreurs
 if (localStorage.getItem("clickCount") === null
     && localStorage.getItem("clickSpeed") === null
     && localStorage.getItem('currentTime') === null)
@@ -29,6 +31,9 @@ if (localStorage.getItem("clickCount") === null
     clickCounterSpeed.textContent = localStorage.getItem("clickSpeed");
     recordGame();
 }
+
+
+
 // intérval pour l'enrégistrement du jeu toutes les 10 seconds
 const gameRecordInterval = setInterval(() => {
     recordGame();
@@ -43,14 +48,15 @@ clickCounterSpan.textContent = userClickCount.toString();
 
 function recordGame() {
     recordCount++;
-    let clickSpeed = localStorage.getItem("clickSpeed");
-    let clickCount = localStorage.getItem("clickCount");
+     clickSpeed = localStorage.getItem("clickSpeed");
+     clickCount = localStorage.getItem("clickCount");
+    (clickCount >= 3 || userClickReset) >= 3 ? shopHolder.classList.remove('hidden') : shopHolder.classList.add('hidden');
     let lastRecordingTime = localStorage.getItem('currentTime');
     let timeDiff = Date.now() - parseInt(lastRecordingTime);
     timeDiff = Math.round(timeDiff / 999);
-    let bonusClicks = (clickSpeed * timeDiff + userClickReset).toString();
+    let bonusClicks = clickSpeed !== 0 ? (clickSpeed * timeDiff + userClickReset) : 0;
     userClickReset = 0;
-    let newCount = ((parseInt(clickCount) + parseInt(bonusClicks))).toString();
+    let newCount = ((parseInt(clickCount) + bonusClicks)).toString();
 
     console.log(recordCount+ " : record made");
     localStorage.setItem('currentTime', Date.now().toString())
